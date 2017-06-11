4c43a21ec73fbb80bf9eeab70d2d14fea80787bc
Redian Ibra <redian@dred.com>
2017-06-11 23:07:25 +0100

    Add react-toolbox and react-toolbox-themr to make it work with create-react-app installation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import theme from './toolbox/theme.js'
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './toolbox/theme.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>, 
+  document.getElementById('root')
+);
 registerServiceWorker();
