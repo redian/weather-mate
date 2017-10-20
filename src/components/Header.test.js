0c4d788e4e68985da6d124485609e17c63e52c53
Redian Ibra <redian@dred.com>
2017-10-20 16:00:32 +0100

    Update the tests to use the global defined utils in the setupTests file.

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from 'enzyme';
 import Header from './Header';
 
 it('renders the AppBar component properly', () => {
--- src/components/Loader.test.js
+++ src/components/Loader.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from 'enzyme';
 import Loader from './Loader';
 
 it('renders the Loader component properly', () => {
